98ea5a9c0de4707728d33a6a28a0d8792b01e6a6
Habib Firas Hadroug <74935553+habibfiras@users.noreply.github.com>
2023-03-07 00:01:01 +0100

    Revert "BIOUP-76-as-a-user-i-want-to-create-account-with-google"

--- src/Presentation/controllers/userController.js
+++ src/Presentation/controllers/userController.js
@@ -1,17 +1,66 @@
 const userService = require('../../Application/UseCases/user/userService');
-const user = require('../../Domain/Entities/user')
+const User = require('../../Infrastructure/Models/userModel');
 
+
+
+//Create New User
 const addUser = async (req, res) => {
   try {
-    const user = req.body; // assuming user details are passed in the request body
-    const createdUser = await userService.addUser(user);
-    res.status(201).json(createdUser);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Internal Server Error' });
+    const userIsExist = (await User.exists({ email: req.body.email })) || null;
+    if (userIsExist) {
+      return res.status(409).send({ error: 'User is already registered' });
+    }
+
+    const user = new User({
+      ...req.body,
+    });
+    await user.save();
+    const token = await user.generateAuthToken();
+    res
+      .header('x-auth-token', token)
+      .header('access-control-expose-headers', 'x-auth-token')
+      .status(201)
+      .send({ token });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send();
   }
 };
 
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params._id);
+    return res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send({ error: e });
+  }
+};
+
+
+const getUsersList= async(req,res)=>{
+try{
+  const users = await User.find().select('firstName password email role');
+  res.send(users);
+}catch(e){
+  res.status(500).send({error:e})
+}
+}
+
+//Desactivate User Account
+
+const DesactivateUserAccount = async (req, res) => {
+  const user = await User.findById(req.params._id);
+  try {
+    user.isActivated = false;
+    await user.save();
+    res.status(200).send({ message: 'Action completed successfully!' });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+
+
 module.exports = {
-  addUser,
+  addUser,getUserById,getUsersList,DesactivateUserAccount
 };
