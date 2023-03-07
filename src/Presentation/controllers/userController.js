9fd5ee7148a9c87e239931face87b97d2d74d2e8
lotfi0310 <lotfilouiz@gmail.com>
2023-03-07 21:13:11 +0100

    userpres

--- src/Presentation/controllers/userController.js
+++ src/Presentation/controllers/userController.js
@@ -90,7 +90,69 @@ const changePass =async (req,res)=>{
   }
 }
 
+ 
+
+
+
+//Create New User
+const addUser = async (req, res) => {
+  try {
+    const userIsExist = (await User.exists({ email: req.body.email })) || null;
+    if (userIsExist) {
+      return res.status(409).send({ error: 'User is already registered' });
+    }
+
+    const user = new User({
+      ...req.body,
+    });
+    await user.save();
+    const token = await user.generateAuthToken();
+    res
+      .header('x-auth-token', token)
+      .header('access-control-expose-headers', 'x-auth-token')
+      .status(201)
+      .send({ token });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send();
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params._id);
+    return res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send({ error: e });
+  }
+}
+
+
+const getUsersList= async(req,res)=>{
+try{
+  const users = await User.find().select('firstName password email role');
+  res.send(users);
+}catch(e){
+  res.status(500).send({error:e})
+}
+}
+
+//Desactivate User Account
+
+const DesactivateUserAccount = async (req, res) => {
+  const user = await User.findById(req.params._id);
+  try {
+    user.isActivated = false;
+    await user.save();
+    res.status(200).send({ message: 'Action completed successfully!' });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+}
+
+
+
 module.exports = {
   sendActivateCodeMail,verifyAccountMail,sendActivateCodeSmS,verifyAccountSms,
-  sendCodeRecBySms,verifyCodeRecBySms,changePass
+sendCodeRecBySms,verifyCodeRecBySms,changePass,getUserById,getUsersList,DesactivateUserAccount,addUser
 };
--- src/Presentation/routes/users.js
+++ src/Presentation/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const auth = require('../middlwares/auth');
+const User = require('../../Infrastructure/Models/userModel');
 
 router.put('/updateactivationcode/:mail',userController.sendActivateCodeMail);
 router.get('/check/activate/account/:token',userController.verifyAccountMail);
@@ -9,6 +11,25 @@ router.get('/check/activate/accountsms/:smscode',userController.verifyAccountSms
 router.put('/updateCodeRecupPassBySms/:phone',userController.sendCodeRecBySms);
 router.get('/check/activate/codeRecupPassBySms/',userController.verifyCodeRecBySms);
 router.put('/changepassword/',userController.changePass);
+
+
+//Create New User
+router.post('/auth/', userController.addUser);
+//Desactivate User Account
+
+router.patch(
+  '/desactivate/:_id',
+  auth,
+  userController.DesactivateUserAccount
+);
+
+
+//Get all users
+router.get('/list',auth,userController.getUsersList)
+
+//Consult User
+router.get('/:_id',auth, userController.getUserById);
+
 router.get('/', function(req, res, next) {
   res.send('users list page !');
 });
