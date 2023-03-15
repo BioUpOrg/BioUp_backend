a299e21f080b263ad84266446e05f39fba9576d2
Habib Firas Hadroug <74935553+habibfiras@users.noreply.github.com>
2023-03-15 05:50:20 +0100

    fix issue with fb login

--- src/Presentation/middlwares/auth.js
+++ src/Presentation/middlwares/auth.js
@@ -5,15 +5,23 @@ const User = require('../../Infrastructure/Models/userModel');
 const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, process.env.PRIVATEKEY);
+    console.log("1 "+token);
+    if (token.startsWith('"')) {
+     var result= token.substring(1, token.length-1);
+
+    }else{
+      var result=token;
+    }
+console.log("2 "+token);
+    var decoded = jwt.verify(result, process.env.PRIVATEKEY);
     const user = await User.findOne({
       _id: decoded._id,
-      'tokens.token': token,
+      'tokens.token': result,
     });
     if (!user) {
       throw new Error();
     }
-    req.token = token;
+    req.token = result;
     req.user = user;
     next();
   } catch (e) {
