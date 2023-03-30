52e4aa1002fe1c7b7891e053cfe14df45a85b43e
lotfi0310 <lotfilouiz@gmail.com>
2023-03-30 04:37:17 +0200

    BIOUP-101-as-a-user-i-want-to-see-my-contract-details-backend

--- src/Application/UseCases/contract/contratService.js
+++ src/Application/UseCases/contract/contratService.js
@@ -9,4 +9,14 @@ const addContract = async (c)=>{
     }
 
 }
-module.exports={addContract};
+const FindContractByUserID=async(userid)=>{
+    try{
+        console.log("serv c id ", userid);
+        const contract=await Contract.findOne({user:userid});
+        return contract;
+    }
+    catch(error){
+        console.log(error,"could not find a contract by user id")
+    }
+}
+module.exports={addContract,FindContractByUserID};
