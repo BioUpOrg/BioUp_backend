3e8a33e915c9ff5948d7fa9760732b880cc70196
tarek008 <tarek.ayadi@esprit.tn>
2023-04-16 02:58:31 +0100

    FixError_in_Animals_Work_done

--- src/Presentation/routes/animals.js
+++ src/Presentation/routes/animals.js
@@ -2,47 +2,10 @@ const express = require('express');
 const router = express.Router();
 const animalModel = require('../../Infrastructure/Models/animalModel');
 const animalController = require('../controllers/animalController');
-const vision = require('@google-cloud/vision');
-const omit = require('../utils/omit');
-const uploadImage = require('../utils/cloudinary/uploadImage');
-const fs = require('fs');
-const path = require('path');
 
 
-const client = new vision.ImageAnnotatorClient({
-    keyFilename: './GoogleCloudVisionApiKey.json'
-});
-
-  router.get('/getIAObjects', async (req, res) => {
-        try {
-            if (Object.keys(req.files || {}).length > 0) {
-
-                const image = req.files.file[0] || req.body.file || { path: '' };
-                const uploadedImage = await uploadImage(image.path);            
-                 pic = uploadedImage ? uploadedImage.url : '';
-                if (uploadedImage) {
-                  let filePath = path.join(`${__dirname}/../../`, image.path);
-                  if (filePath.includes('uploads')) {
-                    fs.unlink(filePath, () => {});
-                  }
-                }
-              }
-
-            const [result] = await client.objectLocalization(pic);
-            const objects = result.localizedObjectAnnotations.map(object => ({
-           name: object.name,
-            score: object.score,
-            boundingPoly: object.boundingPoly
-            }));
-            res.json(objects);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Error detecting objects');
-        }
-});
+router.post('/getIAObjects', animalController.getIAObjects);
   
-
-
 
 //Getting all
 router.get('/', animalController.getAllAnimals);
