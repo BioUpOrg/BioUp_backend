d8c03e504a5ec61938fd29fddee9f86773c65731
lotfi0310 <lotfilouiz@gmail.com>
2023-04-26 13:43:10 +0200

    update my location on change done

--- src/Presentation/routes/shipmentroute.js
+++ src/Presentation/routes/shipmentroute.js
@@ -6,4 +6,5 @@ const shipmentController =require('../controllers/shipmentController');
 router.get('/listnotDelivred',shipmentController.findCommandesNotDelivered);
 router.post('/addnewShipment',shipmentController.addShipment);
 router.get('/MyMission/:id',shipmentController.getMyshipment);
+router.put('/updateMylocation/:agent_id',shipmentController.updateMylocation);
 module.exports = router;
