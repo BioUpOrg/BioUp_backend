86efbe885a80191fe95d38d3b965d1509ab49bfc
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-05-07 17:00:08 +0200

    modif deploy

--- src/Presentation/routes/ForgetPasswordMail.js
+++ src/Presentation/routes/ForgetPasswordMail.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const forgetPasswordMail = require('../controllers/ForgetPasswordMail');
+const compostsController = require('../controllers/compostsController');
+const auth = require('../middlwares/auth');
 
-router.put('/' , forgetPasswordMail.sendps);
-router.put('/verif',forgetPasswordMail.verifps );
-router.put('/changeps', forgetPasswordMail.changeps);
+//Get all composts
+router.get('/all', compostsController.getAllComposts);
+
+//Get seller composts
+router.get('/GetSellerComposts',auth, compostsController.getSellerComposts);
+
+//Get top rated composts
+router.get('/topRated', compostsController.getTopRatedComposts);
+
+//Get recently added composts
+router.get('/recentlyAdded', compostsController.getRecentlyAddedComposts);
+
+//Get recently added composts
+router.get('/topSelled', compostsController.getTopSelledComposts);
+
+//Get a compost details by id
+router.get('/:id', compostsController.getCompostById);
+
+//Add new compost
+router.post('/', auth,compostsController.addCompost);
+
+//update a compost by id
+router.put('/:id', compostsController.updateCompost);
+
+//delete a compost by id
+router.delete('/:id', compostsController.deleteCompost);
+
+//recommend composts based on soil type
+router.get('/recommend/:soilType', compostsController.getRecommendedComposts);
 
 module.exports = router;
-
-//
-//
