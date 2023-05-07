09fc67098427d9d0edc27dfbce97c3bd5edf4130
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-05-07 16:27:19 +0200

    modif deploy

--- src/Presentation/routes/ForgetPasswordMail.js
+++ src/Presentation/routes/ForgetPasswordMail.js
@@ -2,9 +2,9 @@ const express = require('express');
 const forgetRouter = express.Router();
 const forgetPasswordMail = require('../controllers/ForgetPasswordMail');
 
-forgetRouter.put('/' , forgetPasswordMail.sendps);
-forgetRouter.put('/verif',forgetPasswordMail.verifps, );
-forgetRouter.put('/changeps', forgetPasswordMail.changeps);
+// forgetRouter.put('/' , forgetPasswordMail.sendps);
+// forgetRouter.put('/verif',forgetPasswordMail.verifps, );
+// forgetRouter.put('/changeps', forgetPasswordMail.changeps);
 
 module.exports = forgetRouter;
 
